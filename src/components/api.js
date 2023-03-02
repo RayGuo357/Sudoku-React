ae3414169811d097ec125083e8e6f92f77c9316a
Raymond Guo <45112078+RayGuo357@users.noreply.github.com>
2023-03-02 11:57:38 -0500

    api styling

--- src/components/api.js
+++ src/components/api.js
@@ -4,14 +4,17 @@ export default function Api() {
 
     return (
         <div className="container">
-            <div> API </div>
-            <code>curl -w something</code>
+            <h3> getAll </h3>
+            <pre><code>curl -v {process.env.API_LINK === undefined ? "{Missing Link}" : process.env.API_LINK}/api/getAll</code></pre>
             <hr />
-            <div> API </div>
-            <code>curl -w something</code>
+            <h3> getRandom </h3>
+            <pre><code>curl -v {process.env.API_LINK === undefined ? "{Missing Link}" : process.env.API_LINK}/api/getRandom</code></pre>
             <hr />
-            <div> API </div>
-            <code>curl -w something</code>
+            <h3> getDaily </h3>
+            <pre><code>curl -v {process.env.API_LINK === undefined ? "{Missing Link}" : process.env.API_LINK}/api/getDaily/:date</code></pre>
+            <hr />
+            <h3> postBoard </h3>
+            <pre><code>curl -d 'author=asdfasdf' {process.env.API_LINK === undefined ? "{Missing Link}" : process.env.API_LINK}/api/postBoard</code></pre>
             <hr />
         </div>
     )
